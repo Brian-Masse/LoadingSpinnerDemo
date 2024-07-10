6a34b63a8c2e90d1b46c7280b1929c8483d9b59b
Brian Masse <brianm25it@gmail.com>
2024-07-09 22:39:08 -0400

    inital commit

---
+++ LoadingSpinnerDemo/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  LoadingSpinnerDemo
+//
+//  Created by Brian Masse on 7/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    var body: some View {
+        Text("I didnt know!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LoadingSpinnerDemo/LoadingSpinnerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoadingSpinnerDemoApp.swift
+//  LoadingSpinnerDemo
+//
+//  Created by Brian Masse on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct LoadingSpinnerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
